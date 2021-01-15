3c4a307df3de130555360968082332469ad7cce1
qingquan <qingquan@inwil.com>
2021-01-15 10:57:07 +0800

    vue2 模板脱离index.html文件

--- src/main.js
+++ src/main.js
@@ -3,14 +3,9 @@ import Vue from 'vue'
 
 
 const app = new Vue({
-  // el: "#app",
-  template : "<div class='a b c'>{{name}}</div>",
+  el: "#app",
+  template : "<div class='a b c'> Hello {{name}} </div>",
   data: {
     name : "liuayong"
   }
 })
-
-console.log(app)
-app.$mount();
-console.log(app.$el)
-document.getElementById('app').appendChild(app.$el)
